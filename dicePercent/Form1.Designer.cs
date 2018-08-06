134af1316d697c283ac7563393b3538b4270aad5
willi <willi@LAPTOP-QF79BDNT>
2018-08-06 20:57:36 -0300

    Adicionar .gitignore e .gitattributes.

---
+++ dicePercent/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace dicePercent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.diePic1 = new System.Windows.Forms.PictureBox();
+            this.diePic2 = new System.Windows.Forms.PictureBox();
+            this.diePic3 = new System.Windows.Forms.PictureBox();
+            this.diePic4 = new System.Windows.Forms.PictureBox();
+            this.diePic5 = new System.Windows.Forms.PictureBox();
+            this.diePic6 = new System.Windows.Forms.PictureBox();
+            this.diePic7 = new System.Windows.Forms.PictureBox();
+            this.diePic8 = new System.Windows.Forms.PictureBox();
+            this.diePic9 = new System.Windows.Forms.PictureBox();
+            this.diePic10 = new System.Windows.Forms.PictureBox();
+            this.diePic11 = new System.Windows.Forms.PictureBox();
+            this.diePic12 = new System.Windows.Forms.PictureBox();
+            this.rollDice = new System.Windows.Forms.Button();
+            this.displayTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic12)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // diePic1
+            // 
+            this.diePic1.Image = global::dicePercent.Properties.Resources.die6;
+            this.diePic1.Location = new System.Drawing.Point(32, 41);
+            this.diePic1.Name = "diePic1";
+            this.diePic1.Size = new System.Drawing.Size(100, 98);
+            this.diePic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePic1.TabIndex = 0;
+            this.diePic1.TabStop = false;
+            // 
+            // diePic2
+            // 
+            this.diePic2.Image = global::dicePercent.Properties.Resources.die6;
+            this.diePic2.Location = new System.Drawing.Point(161, 41);
+            this.diePic2.Name = "diePic2";
+            this.diePic2.Size = new System.Drawing.Size(100, 98);
+            this.diePic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePic2.TabIndex = 1;
+            this.diePic2.TabStop = false;
+            // 
+            // diePic3
+            // 
+            this.diePic3.Image = global::dicePercent.Properties.Resources.die6;
+            this.diePic3.Location = new System.Drawing.Point(288, 41);
+            this.diePic3.Name = "diePic3";
+            this.diePic3.Size = new System.Drawing.Size(100, 98);
+            this.diePic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePic3.TabIndex = 2;
+            this.diePic3.TabStop = false;
+            // 
+            // diePic4
+            // 
+            this.diePic4.Image = global::dicePercent.Properties.Resources.die6;
+            this.diePic4.Location = new System.Drawing.Point(419, 41);
+            this.diePic4.Name = "diePic4";
+            this.diePic4.Size = new System.Drawing.Size(100, 98);
+            this.diePic4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePic4.TabIndex = 3;
+            this.diePic4.TabStop = false;
+            // 
+            // diePic5
+            // 
+            this.diePic5.Image = global::dicePercent.Properties.Resources.die6;
+            this.diePic5.Location = new System.Drawing.Point(542, 41);
+            this.diePic5.Name = "diePic5";
+            this.diePic5.Size = new System.Drawing.Size(100, 98);
+            this.diePic5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePic5.TabIndex = 4;
+            this.diePic5.TabStop = false;
+            // 
+            // diePic6
+            // 
+            this.diePic6.Image = global::dicePercent.Properties.Resources.die6;
+            this.diePic6.Location = new System.Drawing.Point(672, 41);
+            this.diePic6.Name = "diePic6";
+            this.diePic6.Size = new System.Drawing.Size(100, 98);
+            this.diePic6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePic6.TabIndex = 5;
+            this.diePic6.TabStop = false;
+            // 
+            // diePic7
+            // 
+            this.diePic7.Image = global::dicePercent.Properties.Resources.die6;
+            this.diePic7.Location = new System.Drawing.Point(32, 182);
+            this.diePic7.Name = "diePic7";
+            this.diePic7.Size = new System.Drawing.Size(100, 98);
+            this.diePic7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePic7.TabIndex = 6;
+            this.diePic7.TabStop = false;
+            // 
+            // diePic8
+            // 
+            this.diePic8.Image = global::dicePercent.Properties.Resources.die6;
+            this.diePic8.Location = new System.Drawing.Point(161, 182);
+            this.diePic8.Name = "diePic8";
+            this.diePic8.Size = new System.Drawing.Size(100, 98);
+            this.diePic8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePic8.TabIndex = 7;
+            this.diePic8.TabStop = false;
+            // 
+            // diePic9
+            // 
+            this.diePic9.Image = global::dicePercent.Properties.Resources.die6;
+            this.diePic9.Location = new System.Drawing.Point(288, 182);
+            this.diePic9.Name = "diePic9";
+            this.diePic9.Size = new System.Drawing.Size(100, 98);
+            this.diePic9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePic9.TabIndex = 8;
+            this.diePic9.TabStop = false;
+            // 
+            // diePic10
+            // 
+            this.diePic10.Image = global::dicePercent.Properties.Resources.die6;
+            this.diePic10.Location = new System.Drawing.Point(419, 182);
+            this.diePic10.Name = "diePic10";
+            this.diePic10.Size = new System.Drawing.Size(100, 98);
+            this.diePic10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePic10.TabIndex = 9;
+            this.diePic10.TabStop = false;
+            // 
+            // diePic11
+            // 
+            this.diePic11.Image = global::dicePercent.Properties.Resources.die6;
+            this.diePic11.Location = new System.Drawing.Point(542, 182);
+            this.diePic11.Name = "diePic11";
+            this.diePic11.Size = new System.Drawing.Size(100, 98);
+            this.diePic11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePic11.TabIndex = 10;
+            this.diePic11.TabStop = false;
+            // 
+            // diePic12
+            // 
+            this.diePic12.Image = global::dicePercent.Properties.Resources.die6;
+            this.diePic12.Location = new System.Drawing.Point(672, 182);
+            this.diePic12.Name = "diePic12";
+            this.diePic12.Size = new System.Drawing.Size(100, 98);
+            this.diePic12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diePic12.TabIndex = 11;
+            this.diePic12.TabStop = false;
+            // 
+            // rollDice
+            // 
+            this.rollDice.Location = new System.Drawing.Point(288, 309);
+            this.rollDice.Name = "rollDice";
+            this.rollDice.Size = new System.Drawing.Size(214, 47);
+            this.rollDice.TabIndex = 12;
+            this.rollDice.Text = "Roll Dice";
+            this.rollDice.UseVisualStyleBackColor = true;
+            this.rollDice.Click += new System.EventHandler(this.rollDice_Click);
+            // 
+            // displayTextBox
+            // 
+            this.displayTextBox.Location = new System.Drawing.Point(32, 376);
+            this.displayTextBox.Multiline = true;
+            this.displayTextBox.Name = "displayTextBox";
+            this.displayTextBox.Size = new System.Drawing.Size(740, 163);
+            this.displayTextBox.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 551);
+            this.Controls.Add(this.displayTextBox);
+            this.Controls.Add(this.rollDice);
+            this.Controls.Add(this.diePic12);
+            this.Controls.Add(this.diePic11);
+            this.Controls.Add(this.diePic10);
+            this.Controls.Add(this.diePic9);
+            this.Controls.Add(this.diePic8);
+            this.Controls.Add(this.diePic7);
+            this.Controls.Add(this.diePic6);
+            this.Controls.Add(this.diePic5);
+            this.Controls.Add(this.diePic4);
+            this.Controls.Add(this.diePic3);
+            this.Controls.Add(this.diePic2);
+            this.Controls.Add(this.diePic1);
+            this.Name = "Form1";
+            this.Text = "Percent of Die number!";
+            ((System.ComponentModel.ISupportInitialize)(this.diePic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diePic12)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox diePic1;
+        private System.Windows.Forms.PictureBox diePic2;
+        private System.Windows.Forms.PictureBox diePic3;
+        private System.Windows.Forms.PictureBox diePic4;
+        private System.Windows.Forms.PictureBox diePic5;
+        private System.Windows.Forms.PictureBox diePic6;
+        private System.Windows.Forms.PictureBox diePic7;
+        private System.Windows.Forms.PictureBox diePic8;
+        private System.Windows.Forms.PictureBox diePic9;
+        private System.Windows.Forms.PictureBox diePic10;
+        private System.Windows.Forms.PictureBox diePic11;
+        private System.Windows.Forms.PictureBox diePic12;
+        private System.Windows.Forms.Button rollDice;
+        private System.Windows.Forms.TextBox displayTextBox;
+    }
+}
+
---
+++ dicePercent/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dicePercent
+{
+    public partial class Form1 : Form
+    {
+        private int ones = 0, twos = 0, threes = 0, fours = 0, fives = 0, sixes = 0;
+
+        private void rollDice_Click(object sender, EventArgs e)
+        {
+            DisplayDie(diePic1);
+            DisplayDie(diePic2);
+            DisplayDie(diePic3);
+            DisplayDie(diePic4);
+            DisplayDie(diePic5);
+            DisplayDie(diePic6);
+            DisplayDie(diePic7);
+            DisplayDie(diePic8);
+            DisplayDie(diePic9);
+            DisplayDie(diePic10);
+            DisplayDie(diePic11);
+            DisplayDie(diePic12);
+
+            double total = ones + twos + threes + fours + fives + sixes;
+
+            displayTextBox.Text = "Face\t\tFrequencia\tPorcentagem" + Environment.NewLine + 
+                "1\t\t" + ones + "\t\t" +
+                String.Format("{0:F2}", ones / total * 100) +
+                "%" + Environment.NewLine + "2\t\t" + twos + "\t\t" +
+                String.Format("{0:F2}", twos / total * 100) +
+                "%" + Environment.NewLine + "3\t\t" + threes + "\t\t" +
+                String.Format("{0:F2}", threes / total * 100) +
+                "%" + Environment.NewLine + "4\t\t" + fours + "\t\t" +
+                String.Format("{0:F2}", fours / total * 100) +
+                "%" + Environment.NewLine + "5\t\t" + fives + "\t\t" +
+                String.Format("{0:F2}", fives / total * 100) +
+                "%" + Environment.NewLine + "6\t\t" + sixes + "\t\t" +
+                String.Format("{0:F2}", sixes / total * 100) + "%";
+        }
+
+        private Random randomNumber = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplayDie( PictureBox diePic)
+        {
+            int face = randomNumber.Next(1, 7);
+            switch (face)
+            {
+                case 1:
+                    diePic.Image = Properties.Resources.die1;
+                    ones++;
+                    break;
+                case 2:
+                    diePic.Image = Properties.Resources.die2;
+                    twos++;
+                    break;
+                case 3:
+                    diePic.Image = Properties.Resources.die3;
+                    threes++;
+                    break;
+                case 4:
+                    diePic.Image = Properties.Resources.die4;
+                    fours++;
+                    break;
+                case 5:
+                    diePic.Image = Properties.Resources.die5;
+                    fives++;
+                    break;
+                case 6:
+                    diePic.Image = Properties.Resources.die6;
+                    sixes++;
+                    break;
+            }
+        }
+    }
+}
